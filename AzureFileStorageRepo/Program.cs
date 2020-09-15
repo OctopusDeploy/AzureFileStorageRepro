2168da985f3b86ce70387f26fa72592608ea3b71
Frank Lin <frank.lin@octopus.com>
2020-09-15 16:36:38 +1000

    Add filling

--- AzureFileStorageRepo/Program.cs
+++ AzureFileStorageRepo/Program.cs
@@ -1,18 +1,71 @@
 ﻿using System;
 using System.IO;
-using System.Net;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace AzureFileStorageRepo
 {
     class Program
     {
+        private const int _bytesIn10MB = 1024 * 1024 * 10;
+
         static void Main(string[] args)
         {
-            string srcFilePath = args[0];
-            string destFilePath = args[1];
+            var command = args[0];
+            var rootPath = args[1];
 
-            CopyFileAndVerify(srcFilePath, destFilePath);
+            if (command == "fill")
+            {
+                FillStorage(rootPath);
+            }
+            else if (command == "repro")
+            {
+                ExceedQuota(rootPath);
+            }
+            else
+            {
+                throw new Exception("Command must be 'fill' or 'repro', and the path to the file share");
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        private static void ExceedQuota(string rootPath)
+        {
+            var fileName = "ReproFile";
+            var srcPath = Path.GetFullPath(fileName);
+            File.WriteAllBytes(srcPath, Generate10MBFile());
+
+            CopyFileAndVerify(srcPath, Path.Combine(rootPath, fileName));
+        }
+
+        private static void FillStorage(string rootPath)
+        {
+            var pathForDummyFiles = Path.Combine(rootPath, "dummy");
+            var bytesInTotal = 1024 * 1024 * 1024;
+
+            var filesNeeded = bytesInTotal / _bytesIn10MB;
+
+            Directory.CreateDirectory(pathForDummyFiles);
+            foreach (var fileNumber in Enumerable.Range(1, filesNeeded))
+            {
+                var fileName = Path.Combine(pathForDummyFiles, "dummy" + fileNumber);
+                if (File.Exists(fileName))
+                {
+                    Console.WriteLine("Dummy file already exists: " + fileName);
+                }
+                else
+                {
+                    Console.WriteLine("Creating dummy file: " + fileName);
+                    File.WriteAllBytes(fileName, Generate10MBFile());
+                }
+            }
+        }
+
+        private static byte[] Generate10MBFile()
+        {
+            return Enumerable.Repeat((byte) 65, _bytesIn10MB).ToArray();
         }
 
         private static void CopyFileAndVerify(string srcFilePath, string destFilePath)
@@ -41,7 +94,7 @@ namespace AzureFileStorageRepo
         {
             using var fileStream = File.OpenRead(filePath);
             using SHA1CryptoServiceProvider cryptoServiceProvider = new SHA1CryptoServiceProvider();
-            return  BitConverter.ToString(cryptoServiceProvider.ComputeHash(fileStream));
+            return BitConverter.ToString(cryptoServiceProvider.ComputeHash(fileStream));
         }
 
         private static long GetFileSizeFromStream(string filePath)
